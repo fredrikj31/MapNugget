937b3d4af79405f2e8f7044481121cf406e2f7b3
Fredrik Johansen <fredrik1206@gmail.com>
2022-01-02 20:28:22 +0100

    Added support for restaurants in denmark

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,48 +6,85 @@ $(function () {
 
 	var map;
 	var mapRegions = [];
+	var restaurants;
+
+	async function getData() {
+		await $.getJSON('./assets/data/dk/restaurants.json', function (data) {
+			restaurants = data;
+		});
+	}
+
+	getData().then(
+		function (value) {
+			let temp = []
+			restaurants.map(
+				function (h) {
+					//console.log({ name: h.name, latLng: [h.coordinates.lat, h.coordinates.lng] });
+
+					temp.push({ name: h.name, latLng: [h.coordinates.lat, h.coordinates.lng] })
+
+					//return { name: h.name, latLng: [h.coordinates.lat, h.coordinates.lng] } 
+				}
+			),
+
+
+				map = new jvm.Map({
+					map: "dk_mill",
+					markers: temp,
+					markerStyle: {
+						initial: {
+							fill: 'yellow',
+							stroke: 'red',
+						},
+					},
+					regionStyle: {
+						image: "./assets/images/mcdonalds-logo.png",
+					},
+					container: $('#map'),
+					series: {
+						regions: [{
+							scale: {
+								nugget: './assets/images/nugget.png',
+							}
+						}],
+					},
+					labels: {
+						regions: {
+							render: function (code) {
+								//console.log(code);
+
+								mapRegions.push(code);
+							},
+						},
+					},
+					onRegionTipShow: function (e, el) {
+						el.html(el.html());
+					},
+				})
+
+			console.log(map)
+
+			// Setting the color of the map to nugget!
+			for (let i = 0; i < ObjectLength(map.regions); i++) {
+				var data = {}
+				data[mapRegions[i]] = "nugget"
+
+				map.series.regions[0].setValues(data);
+			}
+		},
+		function (error) {
+			alert(error);
+		}
+	)
+
 
 	function ObjectLength(object) {
 		var length = 0;
-		for( var key in object ) {
-			if( object.hasOwnProperty(key) ) {
+		for (var key in object) {
+			if (object.hasOwnProperty(key)) {
 				++length;
 			}
 		}
 		return length;
 	};
-	
-	//map = $('#map').vectorMap({
-	map = new jvm.Map({
-		map: "dk_mill",
-		container: $('#map'),
-		series: {
-			regions: [{
-				scale: {
-					nugget: './assets/images/nugget.png',
-				}
-			}]
-		},
-		labels: {
-			regions: {
-				render: function (code) {
-					//console.log(code);
-
-					mapRegions.push(code);
-				},
-			},
-		},
-		onRegionTipShow: function (e, el) {
-			el.html(el.html());
-		},
-	});
-
-	
-	// Setting the color of the map to nugget!
-	for (let i = 0; i < ObjectLength(map.regions); i++) {
-		var data = {}
-		data[mapRegions[i]] = "nugget"
-
-		map.series.regions[0].setValues(data);
-	}
 });
